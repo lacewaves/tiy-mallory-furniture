2fb60a90a9d00d60b752c5c6ddf5523f022a0fb4
lacewaves <lacewaves@users.noreply.github.com>
2016-02-22 15:03:21 -0600

    Create online shop

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,41 @@
+require 'csv'
+class ProductsController < ApplicationController
+
+  def list
+    @products = furnitureitems
+
+  end
+
+  def detail
+    @products = furnitureitems
+
+     the_id = params[:id]
+
+     @product = @products.find {|product| product.id == params[:id]}
+  end
+
+  def furnitureitems
+
+    @products= []
+
+    CSV.foreach(Rails.root + "data/mf_inventory_csv.csv", headers: true) do |row|
+
+      product = Product.new
+      product.id = row.to_h["pid"]
+      product.item = row.to_h["item"]
+      product.description = row.to_h["description"]
+      product.price = row.to_h["price"]
+      product.condition = row.to_h["condition"]
+      product.dimension_w = row.to_h["dimension_w"]
+      product.dimension_l = row.to_h["dimension_l"]
+      product.dimension_h = row.to_h["dimension_h"]
+      product.img_name = row.to_h["img_name"]
+      product.quantity = row.to_h["quantity"]
+      product.category = row.to_h["category"]
+
+      @products << product
+    end
+
+    @products
+  end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product
+  attr_accessor :id, :item, :description, :price, :condition,
+  :dimension_w, :dimension_l, :dimension_h, :img_name, :quantity, :category
+end
