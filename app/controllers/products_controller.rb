d80f1282e1ff60de3c6d85247c001c3bdf0cb066
lacewaves <lacewaves@users.noreply.github.com>
2016-03-01 13:17:24 -0600

    Filter out products with quantity of 0

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,22 +20,24 @@ class ProductsController < ApplicationController
 
     CSV.foreach(Rails.root + "data/mf_inventory_csv.csv", headers: true) do |row|
 
-      product = Product.new
-      product.id = row.to_h["pid"]
-      product.item = row.to_h["item"]
-      product.description = row.to_h["description"]
-      product.price = row.to_h["price"]
-      product.condition = row.to_h["condition"]
-      product.dimension_w = row.to_h["dimension_w"]
-      product.dimension_l = row.to_h["dimension_l"]
-      product.dimension_h = row.to_h["dimension_h"]
-      product.img_name = row.to_h["img_name"]
-      product.quantity = row.to_h["quantity"]
-      product.category = row.to_h["category"]
 
-      @products << product
-    end
+      if row.to_h["quantity"].to_i > 0
+        product = Product.new
+        product.id = row.to_h["pid"]
+        product.item = row.to_h["item"]
+        product.description = row.to_h["description"]
+        product.price = row.to_h["price"]
+        product.condition = row.to_h["condition"]
+        product.dimension_w = row.to_h["dimension_w"]
+        product.dimension_l = row.to_h["dimension_l"]
+        product.dimension_h = row.to_h["dimension_h"]
+        product.img_name = row.to_h["img_name"]
+        product.quantity = row.to_h["quantity"]
+        product.category = row.to_h["category"]
 
+        @products << product
+      end
+    end 
     @products
   end
 end
